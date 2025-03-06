8be50e6eff3353e1ceb916489c9e51db0100c86e
Alex B <dop.bass-spb@mail.ru>
2025-03-06 17:22:50 +0300

    some changes

---
+++ Views/Popup/SelectTargetPopup.xaml.cs
@@ -0,0 +1,11 @@
+using CommunityToolkit.Maui.Views;
+
+namespace WhmCalcMaui.Views.Popup;
+
+public partial class SelectTargetPopup : Popup
+{
+	public SelectTargetPopup()
+	{
+		InitializeComponent();
+	}
+}
