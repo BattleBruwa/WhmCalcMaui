95c160acccbb83ae3a44bca16cd7231fdb0b567f
Alex B <dop.bass-spb@mail.ru>
2025-04-24 20:14:26 +0300

    splash screen changes

--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -1,11 +1,20 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using static AndroidX.Core.SplashScreen.SplashScreen;
 
 namespace WhmCalcMaui
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [Activity(Theme = "@style/Theme.Animated", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle? savedInstanceState)
+        {
+            // сплеш с анимацией
+            // https://dev.to/icebeam7/animated-splash-screen-in-net-maui-android-2ipg
+            var splash = InstallSplashScreen(this);
+
+            base.OnCreate(savedInstanceState);
+        }
     }
 }
